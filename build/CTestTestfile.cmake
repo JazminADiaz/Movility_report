b6141f60ad96534f00a014f4348cf2719c60eeff
Jazmin Ubuntu <jazminarianadiaz@gmail.com>
2022-08-22 23:08:24 +0200

    hay dos robots y ia

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jazmin/MultiRobots/src
+# Build directory: /home/jazmin/MultiRobots/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("atom")
