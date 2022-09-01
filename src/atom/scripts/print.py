524ac43e80e58f4d6cae00df65b705e16bb823ef
Jazmin Ubuntu <jazminarianadiaz@gmail.com>
2022-09-01 19:15:47 +0200

    pose vel y nombre de un robot

--- src/atom/scripts/print.py
+++ src/atom/scripts/print.py
@@ -2,51 +2,37 @@
 import string
 import rospy
 from gazebo_msgs.msg import ModelStates
-from geometry_msgs.msg import Twist, Quaternion, Point, Vector3, Pose
+from geometry_msgs.msg import Twist, Pose, Quaternion, Point, Vector3
 from std_msgs.msg import String
-import string
 
 
 rospy.init_node('MoveRobots', anonymous=True)
 robot='robot1'
 rate= rospy.Rate(10)
 
-pos= Point()
-orie=Quaternion()
-vel_l=Vector3()
-vel_a=Vector3()
+pub = rospy.Publisher('/gazebo/model_states', ModelStates, queue_size=10)
+pub1 = rospy.Publisher(f'/{robot}_name', String, queue_size=10)
+pub2 = rospy.Publisher(f'/{robot}_Pos', Pose, queue_size=10)
+pub3 = rospy.Publisher(f'/{robot}_Vel', Twist, queue_size=10)
+
 
 def callback(data):
     for a in  range(len(data.name)):
       if data.name[a]== 'robot1':
 
-        
-        position=data.pose[a]
         name=data.name[a]
         pub1.publish(name)
-        pub5.publish(position)
+        position=data.pose[a]
+        pub2.publish(position)
+        velocity=data.twist[a]
+        pub3.publish(velocity)
         rate.sleep()
 
 
         #rospy.loginfo(data.pose[a])
-##        rospy.loginfo(data.name[a])  
 
-
-
-
-  #  print(data.pose.pose) f"Hello, {first_name}!"
-          
-  #  rospy.loginfo(data.pose)            
 
 rospy.Subscriber('/gazebo/model_states', ModelStates, callback)
-pub = rospy.Publisher('/gazebo/model_states', ModelStates, queue_size=10)
-pub1 = rospy.Publisher(f'{robot}/name', String, quxeue_size=10)
-pub2 = rospy.Publisher(f'{robot}/Orientation', Quaternion, queue_size=10)
-pub3 = rospy.Publisher(f'{robot}/LinearVel', Vector3, queue_size=10)
-pub4 = rospy.Publisher(f'{robot}/AngularVel', Vector3, queue_size=10)
-pub5 = rospy.Publisher(f'{robot}/Pose', Pose, queue_size=10)
-#pub1= rospy.Publisher('/gazebo/set_model_state', ModelStates, queue_size=10)
-# spin() simply keeps python from exiting until this node is stopped
-
 rospy.spin()
+
 
