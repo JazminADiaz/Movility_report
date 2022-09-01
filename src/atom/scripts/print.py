9ff1ba8d98a2eda9249c0888eb91e232d82b50bc
Jazmin Ubuntu <jazminarianadiaz@gmail.com>
2022-09-01 18:00:56 +0200

    graba la posicion y el nombre

--- src/atom/scripts/print.py
+++ src/atom/scripts/print.py
@@ -1,42 +1,52 @@
 #!/usr/bin/env python3.8
+import string
 import rospy
 from gazebo_msgs.msg import ModelStates
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Quaternion, Point, Vector3, Pose
+from std_msgs.msg import String
+import string
+
+
+rospy.init_node('MoveRobots', anonymous=True)
+robot='robot1'
+rate= rospy.Rate(10)
+
+pos= Point()
+orie=Quaternion()
+vel_l=Vector3()
+vel_a=Vector3()
 
 def callback(data):
     for a in  range(len(data.name)):
-      if data.name[a]== 'Robot2':
-        vel.linear.x= 0.1
-        vel.angular.z=0.0
+      if data.name[a]== 'robot1':
 
-        pub.publish(vel)
+        
+        position=data.pose[a]
+        name=data.name[a]
+        pub1.publish(name)
+        pub5.publish(position)
         rate.sleep()
+
+
         #rospy.loginfo(data.pose[a])
-        rospy.loginfo(data.name[a])  
+##        rospy.loginfo(data.name[a])  
 
 
 
 
-  #  print(data.pose.pose)
+  #  print(data.pose.pose) f"Hello, {first_name}!"
           
   #  rospy.loginfo(data.pose)            
 
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-
-rospy.init_node('MoveRobots', anonymous=True)
-
 rospy.Subscriber('/gazebo/model_states', ModelStates, callback)
-pub = rospy.Publisher('/robot2/cmd_vel', Twist, queue_size=10)
+pub = rospy.Publisher('/gazebo/model_states', ModelStates, queue_size=10)
+pub1 = rospy.Publisher(f'{robot}/name', String, quxeue_size=10)
+pub2 = rospy.Publisher(f'{robot}/Orientation', Quaternion, queue_size=10)
+pub3 = rospy.Publisher(f'{robot}/LinearVel', Vector3, queue_size=10)
+pub4 = rospy.Publisher(f'{robot}/AngularVel', Vector3, queue_size=10)
+pub5 = rospy.Publisher(f'{robot}/Pose', Pose, queue_size=10)
 #pub1= rospy.Publisher('/gazebo/set_model_state', ModelStates, queue_size=10)
 # spin() simply keeps python from exiting until this node is stopped
-
-rate= rospy.Rate(10)
-vel = Twist()
 
 rospy.spin()
 
